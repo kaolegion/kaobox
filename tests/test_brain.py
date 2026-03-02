e2b0eec14dc02dbb5208715d9314f39855004936
root <root@kaobox.localdomain>
2026-03-02 16:18:52 +0100

    Add simple test for Brain module

---
+++ tests/test_brain.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TestBrainModule(unittest.TestCase):
+    def test_brain_function(self):
+        # Simuler un test pour une fonction du module Brain
+        self.assertEqual(1 + 1, 2)
+
+if __name__ == '__main__':
+    unittest.main()
