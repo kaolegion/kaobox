ee160050ef525c27a2e11d3571a8a64714135419
kaolegion <kaolegion@proton.me>
2026-03-03 20:44:38 +0100

    chore: remove deprecated test files

--- tests/test_brain.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-class TestBrainModule(unittest.TestCase):
-    def test_brain_function(self):
-        # Simuler un test pour une fonction du module Brain
-        self.assertEqual(1 + 1, 2)
-
-if __name__ == '__main__':
-    unittest.main()
